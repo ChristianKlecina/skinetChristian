50623a5d38e7be1c53c1b87911e607aa0c019cb4
ChristianKlecina <djfntt@yahoo.com>
2022-04-19 23:39:09 +0200

    End of section 9

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,7 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
-import {IProduct} from "./models/product";
-import {IPagination} from "./models/pagination";
+import {IProduct} from "./shared/models/product";
+import {IPagination} from "./shared/models/pagination";
 
 @Component({
   selector: 'app-root',
@@ -11,18 +11,14 @@ import {IPagination} from "./models/pagination";
 export class AppComponent implements OnInit{
   title = 'Skinet yoyoyo';
 
-  products: IProduct[];
 
-  constructor(private http: HttpClient) {
+
+
+
+  constructor() {
   }
 
   ngOnInit(): void {
-    this.http.get('https://localhost:7016/api/products?pageSize=50').subscribe(
-      (response: any) => {
-      this.products = response.data
 
-    }, error => {
-
-    })
   }
 }
